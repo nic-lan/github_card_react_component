867660c172ed312d1f946a471a9a28b4f4b11ab3
nic-lan <narghy82@yahoo.it>
2017-08-22 23:30:36 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+const Card = (props) => {
+ return (
+ 	 <div className="Card">
+     <img width="75" src={props.avatar_url} />
+     <div className="Card__info">
+       <div className="Card__info__title">{props.name}</div>
+       <div>{props.company}</div>
+     </div>
+   </div>
+ );
+};
+
+const CardList = (props) => {
+	return (
+  	<div>
+      {props.cards.map(card => <Card key={card.id} {...card} />)}
+    </div>
+  )
+};
+
+class Form extends React.Component {
+ state = { userName: '' }
+	handleSubmit = (event) => {
+  	event.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then(resp => {
+      	this.props.onSubmit(resp.data);
+        this.setState({userName: ''})
+      });
+  };
+
+  render() {
+  	return (
+    	<form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder="Github username" required />
+        <button type="submit">Add card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+	state = {
+  	cards: []
+  };
+
+  addNewCard = (cardInfo) => {
+  	this.setState(prevState => ({
+    	cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+
+	render() {
+  	return (
+    	<div>
+      <Form onSubmit={this.addNewCard}/>
+      <CardList cards={this.state.cards} />
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
